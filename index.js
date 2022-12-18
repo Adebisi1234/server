41a80be654f9b455741bb1fa2769378b48986b1f
Tobiloba Adebisi <oluwatobilobaadebisi6@gmail.com>
2022-12-18 02:25:29 +0000

    Overtime

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use(function (req, res, next) {
   );
   res.header(
     "Access-Control-Allow-Headers",
+    "access-control-allow-origin",
     "Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization"
   );
   next();
