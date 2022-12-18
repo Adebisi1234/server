40ac70c333565503f4e6064e65c5fba5bc67f406
Tobiloba Adebisi <oluwatobilobaadebisi6@gmail.com>
2022-12-18 00:32:23 +0000

    Almost there

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(function (req, res, next) {
   res.header(
     "Access-Control-Allow-Headers",
     "access-control-allow-origin",
-    "Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization"
+    "Origin, X-Api-Key, X-Requested-With, content-Type, Accept, Authorization"
   );
   next();
 });
