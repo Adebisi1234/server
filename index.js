b0236045c3c04199cdec9cc81600708f8f4179c5
Tobiloba Adebisi <oluwatobilobaadebisi6@gmail.com>
2022-12-18 00:42:04 +0000

    I'm no longer sure of what I'm doing

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(function (req, res, next) {
   );
   res.header(
     "Access-Control-Allow-Headers",
-    "content-type",
+    "Content-Type",
     "access-control-allow-origin",
     "Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization"
   );
